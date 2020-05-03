c0a28996634617ddba87cb6ebb6836ff8fbc54a0
Kevin Yobeth <leokeviny@gmail.com>
2020-05-03 22:53:31 +0700

    [Kevin] Fixed create account keyboard issue, user account is now stored at firestore

--- lib/pages/login_signup_page.dart
+++ lib/pages/login_signup_page.dart
@@ -1,7 +1,16 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:Ever/services/authentication.dart';
 import 'package:Ever/template/colors.dart';
 import 'package:Ever/template/style.dart';
+
+class noGlowBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
+  }
+}
 
 class LoginSignupPage extends StatefulWidget {
   LoginSignupPage({this.auth, this.loginCallback});
@@ -14,11 +23,17 @@ class LoginSignupPage extends StatefulWidget {
 }
 
 class _LoginSignupPageState extends State<LoginSignupPage> {
+  final userDatabase = FirebaseDatabase.instance.reference().child("user");
+
   final _formKey = new GlobalKey<FormState>();
 
   String _email;
   String _password;
   String _errorMessage;
+  String _name;
+  String _phoneNumber;
+  String _dateOfBirth;
+  String _gender;
 
   bool _isLoginForm;
   bool _isLoading;
@@ -28,6 +43,7 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
     final form = _formKey.currentState;
     if (form.validate()) {
       form.save();
+
       return true;
     }
     return false;
@@ -47,8 +63,25 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
           print('Signed in: $userId');
         } else {
           userId = await widget.auth.signUp(_email, _password);
+
           // widget.auth.sendEmailVerification();
           // _showVerifyEmailSentDialog();
+
+          userDatabase.child(userId).set({
+            'userID': userId,
+            'userName': _name,
+            'userEmail': _email,
+            'userPhone': _phoneNumber,
+            'userDOB': _dateOfBirth,
+            'userGender': _gender,
+            'userIdenURL': 'UserIdenURL',
+            'userActiveEvent': null,
+            'userPastEvent': null,
+            'isVerified': 0,
+            'organizationID': null
+          });
+
+          _isLoginForm = !_isLoginForm;
           print('Signed up user: $userId');
         }
         setState(() {
@@ -92,39 +125,61 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      // resizeToAvoidBottomInset: false,
+      // resizeToAvoidBottomPadding: false,
+      body: Stack(
+        alignment: Alignment.bottomCenter,
         children: <Widget>[
-          Stack(
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: <Widget>[
-              Container(
-                height: 350.0,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(20.0),
-                      bottomRight: Radius.circular(20.0)),
-                  color: darkBackgroundColor,
-                ),
+              Stack(
+                children: <Widget>[
+                  Container(
+                    height: _isLoginForm ? 350.0 : 200.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(20.0),
+                          bottomRight: Radius.circular(20.0)),
+                      color: darkBackgroundColor,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: _isLoginForm ? 240.0 : 90.0),
+                    child: Center(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(_isLoginForm ? 'Login' : 'Create Account',
+                              style: header),
+                          Text(
+                              _isLoginForm
+                                  ? 'Please sign in to continue'
+                                  : 'Please sign up to continue',
+                              style: tagline)
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 240.0),
-                child: Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+              Expanded(
+                child: ScrollConfiguration(
+                  behavior: noGlowBehavior(),
+                  child: ListView(
                     children: <Widget>[
-                      Text(_isLoginForm ? 'Login' : 'Create Account',
-                          style: header),
-                      Text(
-                          _isLoginForm
-                              ? 'Please sign in to continue'
-                              : 'Please sign up to continue',
-                          style: tagline)
+                      _showForm(),
+                      showPrimaryButton(),
                     ],
                   ),
                 ),
-              ),
+              )
             ],
           ),
-          _showForm(),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: showSecondaryButton(),
+          )
         ],
       ),
     );
@@ -154,31 +209,41 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
   // }
 
   Widget _showForm() {
-    return new Container(
-      child: new Form(
+    return Container(
+      child: Form(
         key: _formKey,
-        child: new ListView(
-          shrinkWrap: true,
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    showEmailInput(),
-                    showPasswordInput(),
-                    showPrimaryButton(),
-                    showErrorMessage(),
-                  ],
-                ),
-                SizedBox(height: 100.0),
-                showSecondaryButton()
-              ],
-            ),
-          ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[showAll()],
         ),
       ),
     );
+  }
+
+  Widget showAll() {
+    if (_isLoginForm) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          showEmailInput(),
+          showPasswordInput(),
+          showErrorMessage(),
+        ],
+      );
+    } else {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          showUsernameInput(),
+          showEmailInput(),
+          showPasswordInput(),
+          showPhoneNumberInput(),
+          showDateOfBirthInput(),
+          showGenderInput(),
+          showErrorMessage(),
+        ],
+      );
+    }
   }
 
   Widget showErrorMessage() {
@@ -195,7 +260,7 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
         ),
       );
     } else {
-      return new Container(
+      return Container(
         height: 0.0,
       );
     }
@@ -203,7 +268,7 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
 
   Widget showEmailInput() {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 0.0),
+      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
       child: new TextFormField(
         maxLines: 1,
         keyboardType: TextInputType.emailAddress,
@@ -222,7 +287,7 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
 
   Widget showPasswordInput() {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(30.0, 15.0, 30.0, 15.0),
+      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
       child: new TextFormField(
         maxLines: 1,
         obscureText: true,
@@ -239,9 +304,87 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
     );
   }
 
+  Widget showUsernameInput() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
+      child: new TextFormField(
+        maxLines: 1,
+        obscureText: false,
+        autofocus: false,
+        decoration: new InputDecoration(
+            hintText: 'Full Name',
+            icon: new Icon(
+              Icons.lock,
+              color: Colors.grey,
+            )),
+        validator: (value) => value.isEmpty ? 'Fullname can\'t be empty' : null,
+        onSaved: (value) => _name = value.trim(),
+      ),
+    );
+  }
+
+  Widget showPhoneNumberInput() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
+      child: new TextFormField(
+        maxLines: 1,
+        obscureText: false,
+        autofocus: false,
+        decoration: new InputDecoration(
+            hintText: 'Phone Number',
+            icon: new Icon(
+              Icons.lock,
+              color: Colors.grey,
+            )),
+        validator: (value) =>
+            value.isEmpty ? 'Phone number can\'t be empty' : null,
+        onSaved: (value) => _phoneNumber = value.trim(),
+      ),
+    );
+  }
+
+  Widget showDateOfBirthInput() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
+      child: new TextFormField(
+        maxLines: 1,
+        obscureText: false,
+        autofocus: false,
+        decoration: new InputDecoration(
+            hintText: 'Date of Birth',
+            icon: new Icon(
+              Icons.lock,
+              color: Colors.grey,
+            )),
+        validator: (value) =>
+            value.isEmpty ? 'Date of Birth can\'t be empty' : null,
+        onSaved: (value) => _dateOfBirth = value.trim(),
+      ),
+    );
+  }
+
+  Widget showGenderInput() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
+      child: new TextFormField(
+        maxLines: 1,
+        obscureText: false,
+        autofocus: false,
+        decoration: new InputDecoration(
+            hintText: 'Gender',
+            icon: new Icon(
+              Icons.lock,
+              color: Colors.grey,
+            )),
+        validator: (value) => value.isEmpty ? 'Gender can\'t be empty' : null,
+        onSaved: (value) => _gender = value.trim(),
+      ),
+    );
+  }
+
   Widget showPrimaryButton() {
     return new Padding(
-      padding: EdgeInsets.fromLTRB(220.0, 0, 30.0, 0.0),
+      padding: EdgeInsets.fromLTRB(250.0, 10.0, 30.0, 0.0),
       child: SizedBox(
         height: 40.0,
         child: new RaisedButton(
@@ -258,26 +401,23 @@ class _LoginSignupPageState extends State<LoginSignupPage> {
   }
 
   Widget showSecondaryButton() {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 10.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            _isLoginForm
-                ? 'Don\'t have an account? '
-                : 'Already have an account? ',
-            style: bottomText,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          _isLoginForm
+              ? 'Don\'t have an account? '
+              : 'Already have an account? ',
+          style: bottomText,
+        ),
+        InkWell(
+          child: Text(
+            _isLoginForm ? 'Sign Up' : 'Sign In',
+            style: bottomSign,
           ),
-          InkWell(
-            child: Text(
-              _isLoginForm ? 'Sign Up' : 'Sign In',
-              style: bottomSign,
-            ),
-            onTap: toggleFormMode,
-          ),
-        ],
-      ),
+          onTap: toggleFormMode,
+        ),
+      ],
     );
   }
 }
